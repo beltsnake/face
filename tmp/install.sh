5e3697615e92f9e2cf672b136aa585846d439f26
beltsnake <byrondenham@gmail.com>
2016-01-15 23:29:32 +0000

    Update install.sh
    added sudo permissions for apt

--- tmp/install.sh
+++ tmp/install.sh
@@ -3,34 +3,33 @@
 #Adding repo's
 sudo su
 echo "Getting repos"
-sudo dpkg --add-architecture i386 
-add-apt-repository ppa:webupd8team/y-ppa-manager
-apt-get update
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+sudo apt-get update
 sudo add-apt-repository ppa:wine/wine-builds
-apt-get update
+sudo apt-get update
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-apt-get update
+sudo apt-get update
 sudo add-apt-repository ppa:openshot.developers/ppa
-apt-get update
+sudo apt-get update
 
 
 #Installing packages
 echo "Installing software"
-apt-get install skype
-apt-get install yad
-apt-get install --install-recommends winehq-devel
-apt-get install fglrx
-apt-get install xubuntu-desktop
-apt-get install git
-apt-get install openshot openshot-doc
-apt-get install minidlna
+sudo apt-get install skype
+sudo apt-get install yad
+sudo apt-get install --install-recommends winehq-devel
+sudo apt-get install fglrx
+sudo apt-get install xubuntu-desktop
+sudo apt-get install git
+sudo apt-get install openshot openshot-doc
+sudo apt-get install minidlna
 mkdir testgit
 cd testgit
 git init
 git config --global user.name "beltsnake"
 git config --global user.email "byrondenham@gmail.com"
 git pull https://github.com/test-thing/face.git master
-apt-get install steam
+sudo apt-get install steam
 
 sudo restart
 
